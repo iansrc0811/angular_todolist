ac68faa2248cf2128c7c7d56395d2f9a836d2aff
ian yeh <iansrc0811@hotmail.com>
2017-06-10 11:39:44 +0800

    two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  inputHint = '請輸入項目名稱';
+  inputItem = '';
+  todos: any[] = [];
 
+  setInput() {
+    if (this.inputItem) {
+      this.todos.push(this.inputItem);
+      this.inputItem = '';
+    }
+  }
 }
